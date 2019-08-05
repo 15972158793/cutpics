725cd9d520db715c98fab132364fbc744cdef642
295738188@qq.com <15972158793@163.com>
2019-08-05 15:12:45 +0800

    支持横向和纵向切割

--- cut.py
+++ cut.py
@@ -4,13 +4,19 @@
 
 import os
 from PIL import Image
+#图片的名字
+picName = "di2"
+#子文件名称
+picZName = "di_"
+#图片和脚本所在的目录
+baseDir = "C:\\Users\\Administrator\\Desktop\\cutpics\\" + picName
 
-def cut(w,h):
+#横向切割  需要指定切割后保存的目录以及图片名称
+def cutHorizontal(w,h,maxW,maxH):
     #图片的路径
-    name1 = "C:\\Users\\Administrator\\Desktop\\jb\\tt1.png"
-	#切割后的存储位置
-    name2 = "C:\\Users\\Administrator\\Desktop\\jb\\tt\\tt_"
-	
+    name1 = baseDir + ".png"
+    #切割后的存储位置
+    name2 = baseDir + "\\" + picZName
     im =Image.open(name1)
 
     #偏移量
@@ -23,11 +29,9 @@ def cut(w,h):
     y1 = 0
     x2 = dx
     y2 = dy
-	
-	#横向
-    while y2 <= 40:
-        #横向切
-        while x2 <= 560:
+
+    while y2 <= maxH:
+        while x2 <= maxW:
             name3 = name2 + str(n) + ".png"
             #print n,x1,y1,x2,y2
             im2 = im.crop((x1, y1, x2, y2))
@@ -43,27 +47,47 @@ def cut(w,h):
     print u"图片切割成功，切割得到的子图片数为".encode('gbk')
     return n-1
 
-'''
-    #纵向
-    while x2 <= 560:
-        #横向切
-        while y2 <= 300:
+#纵向切割
+def cutPortrait(w,h,maxW,maxH):
+    #图片的路径
+    name1 = baseDir + ".png"
+    #切割后的存储位置
+    name2 = baseDir + "\\" + picZName
+    im =Image.open(name1)
+    #偏移量
+    dx = w
+    dy = h
+    n = 1
+
+    #左上角切割
+    x1 = 0
+    y1 = 0
+    x2 = dx
+    y2 = dy
+
+    while x1 <= maxW:
+        while y1 <= maxH:
             name3 = name2 + str(n) + ".png"
             #print n,x1,y1,x2,y2
-            im2 = im.crop((y1, x1, y2, x2))
-			
-			#save file
+            im2 = im.crop((x1, y1, x2, y2))
+            #save file
             im2.save(name3)
             y1 = y1 + dy
-            y2 = y1 + vy
+            y2 = y1 + dy
             n = n + 1
         x1 = x1 + dx
-        x2 = x1 + vx
+        x2 = x1 + dx
         y1 = 0
-        y2 = vy
-'''
+        y2 = dy
+    print u"图片切割成功，切割得到的子图片数为".encode('gbk')
+    return n-1
+
 
 if __name__=="__main__":
-    #设定切割的宽和高
-    res = cut(14,20)
+    #设定切割的宽和高 最大的宽和高
+    #横向切割
+    #res = cutHorizontal(14,20,560,40)
+
+    #纵向切割
+    res = cutPortrait(337,52,337,144)
     print res
